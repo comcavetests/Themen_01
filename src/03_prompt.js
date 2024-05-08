4d0da21d67cbacc60436c3c0f9db33899b92fb86
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-08 10:36:56 +0000

    prompt dl + ignore

--- src/03_prompt.js
+++ src/03_prompt.js
@@ -4,3 +4,4 @@ const prompt = require('prompt-sync')({sigint: true});
 let firstName = prompt("Vorname?: ");
 let familyName = prompt("Name?: ");
 console.log("Hallo, " + firstName + " " + familyName +"!");
+
