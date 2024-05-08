74f01a6f20a4a7e2122640378145fe3595266e5d
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-08 13:24:00 +0000

    Codechallenge_ende

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -9,7 +9,16 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+
+console.log("Schönen guten Tag, ich bin dein Persönlicher Taschenrechner.");
+console.log("Wollen wir es mal versuchen?")
 const prompt1 = require('prompt-sync')({sigint: true});
+const userInput1 = prompt1("Bitte gib mir deine erste Zahl: ");
+const a = parseFloat(userInput1);
+
 const prompt2 = require('prompt-sync')({sigint: true});
-let summe = prompt1 + prompt2;
-console.log("Die Summe der Zahlen ist:" , summe);
+const userInput2 = prompt1("Bitte gib mir deine zweite Zahl damit ich anfangen kann. ");
+const b = parseFloat(userInput2);
+let c = a + b;
+console.log("Die Summe der Zahlen ist:" , c);
+
