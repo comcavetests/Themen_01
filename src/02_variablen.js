32b6e4b04d3343567080fe48a41dcc9c6b3caad4
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-08 08:34:23 +0000

    V2

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,1 +1,17 @@
 // Zeilenkommentar | STRG + #
+
+/**
+ * 
+ * Block-Kommentar
+ * 
+ /**** Variablen 01 ****/
+ //console.log("hi");
+ 
+ /* Deklaration + Wertezuweisung I */
+
+ let firstName; // Deklaration (Difinition)
+ console.log(firstName); // Ausgabe
+ firstName = "Max"; //Variable festlegen wertzuweisung | assignment 
+ let familyName = "Mütze"; // wertezuweisung Combieniert
+ console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Hi, ich bin Max Mützel
+
