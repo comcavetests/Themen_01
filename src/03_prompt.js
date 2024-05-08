8036fdcb8c7eabe7bf40b9b5f422864c8dfb1c91
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-08 10:12:07 +0000

    New Prompt

---
+++ src/03_prompt.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+// Alles muss man selber machen lassen! ...
+let firstName = prompt("Vorname?: ");
+let familyName = prompt("Name?: ");
+console.log("Hallo, " + firstName + " " + familyName +"!");
