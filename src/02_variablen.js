f25fa11446fc9f4cfefb6be68e6992b1503165da
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-08 10:02:01 +0000

    v1

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -19,6 +19,7 @@
  * JS ist ein untypisierte Sprache
  */
 //----------------------------------------------------------------------
+
 // let test;
 // console.log("Inhalt: "+ test); //ausgabe inhalt
 // console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
@@ -42,14 +43,13 @@
 // /** Variablen vs. Konstanten in JS */
 
 //----------------------------------------------------------------------
+
 //variable
 let test; // Variable : Deklaration
 test = "hi"; //wertzuweisung (Initialisierung)
 test = "Hello"; // Variable 1 (zeile 46) Überschreiben
 console.log("Inhalt: " + test); // Ausgabe
 // Konstante
-const test2 = "Hi!";
+const test2 = "Hi!"; // Deklaration + Wertezuweisung gemeinsam!
 //test2 = "Hello"; // RuntimeError (versuch eine Konstante zu ändern)
 console.log("Inhalt: " , test2);
-
-
