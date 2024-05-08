5a9d7495ce18a02a56268a3ac359b82e0d9be504
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-08 13:53:47 +0000

    codechallenge in schön

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -9,16 +9,19 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
-
-console.log("Schönen guten Tag, ich bin dein Persönlicher Taschenrechner.");
-console.log("Wollen wir es mal versuchen?")
+console.log("**********************************************************************");
+console.log("*****Schönen guten Tag, ich bin dein Persönlicher Taschenrechner.*****");
+console.log("*****************Wollen wir es mal versuchen?*************************");
 const prompt1 = require('prompt-sync')({sigint: true});
 const userInput1 = prompt1("Bitte gib mir deine erste Zahl: ");
 const a = parseFloat(userInput1);
+console.log("**********************************************************************");
 
 const prompt2 = require('prompt-sync')({sigint: true});
-const userInput2 = prompt1("Bitte gib mir deine zweite Zahl damit ich anfangen kann. ");
+const userInput2 = prompt1("und nun gib mir Bitte Deine zweite Zahl damit ich anfangen kann. ");
 const b = parseFloat(userInput2);
 let c = a + b;
-console.log("Die Summe der Zahlen ist:" , c);
+console.log("**********************************************************************");
+console.log("              Die Summe aus :" ,a ,"+" ,b ,"ist ", c);
+console.log("**********************************************************************");
 
