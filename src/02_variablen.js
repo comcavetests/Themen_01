0db6c24705ab6f2103860d446a7808e8f9a79107
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-08 09:53:03 +0000

    Variablen update

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -18,25 +18,38 @@
  * Wertezuweisung II + Datentypen
  * JS ist ein untypisierte Sprache
  */
+//----------------------------------------------------------------------
+// let test;
+// console.log("Inhalt: "+ test); //ausgabe inhalt
+// console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
+// console.log("--------------------");
 
-let test;
-console.log("Inhalt: "+ test); //ausgabe inhalt
-console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
-console.log("--------------------");
+// test = "hi"; // text in "" = string
+// console.log("Inhalt: "+ test); //ausgabe inhalt
+// console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
+// console.log("--------------------");
 
-test = "hi"; // text in "" = string
-console.log("Inhalt: "+ test); //ausgabe inhalt
-console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
-console.log("--------------------");
+// test = 2; // number
+// console.log("Inhalt: "+ test); //ausgabe inhalt
+// console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
+// console.log("--------------------");
 
-test = 2; // number
-console.log("Inhalt: "+ test); //ausgabe inhalt
-console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
-console.log("--------------------");
+// test = true; // wahrheitswert  Boolean True and false
+// console.log("Inhalt: "+ test); //ausgabe inhalt
+// console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
+// console.log("--------------------");
 
-test = true; // wahrheitswert  Boolean True and false
-console.log("Inhalt: "+ test); //ausgabe inhalt
-console.log("Datentyp: "+ typeof test); //ausgabe Datentyp
-console.log("--------------------");
+// /** Variablen vs. Konstanten in JS */
+
+//----------------------------------------------------------------------
+//variable
+let test; // Variable : Deklaration
+test = "hi"; //wertzuweisung (Initialisierung)
+test = "Hello"; // Variable 1 (zeile 46) Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+// Konstante
+const test2 = "Hi!";
+//test2 = "Hello"; // RuntimeError (versuch eine Konstante zu ändern)
+console.log("Inhalt: " , test2);
 
 
