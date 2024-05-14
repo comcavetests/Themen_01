da5421b45b0b698844e47b7b2455315b3605d2ce
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-14 09:07:04 +0000

    zwischenspeicherung

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -7,13 +7,39 @@
 1. Dateneingabe + -überprüfung :  
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : Check!
 */
 
+// Module: addition a + b | test:
+//output(add(2,2));
+//output(add(2,-2));
+//output(add(2,0));
 
+function add(a,b) {
+	return a + b;
+}
 
+//Module: Subtraction 1 - b | test:
+//output(Subtraction(3,2));
+//output(Subtraction(3,-2));
+//output(Subtraction(3,0));
+//output(Subtraction(0,2));
 
+function Subtraction(a , b) {
+	return a - b;
+}
 
+//Module: Multiplikation a*b | test:
+
+function Multiplikation(a,b) {
+	return a * b;
+}
+
+//Module Division a/b |test:
+
+function Division(a,b) {
+	return a / b;
+}
 
 
 
