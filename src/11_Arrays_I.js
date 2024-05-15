6dabd783de23232bca40fcfda86fa123d1efa75d
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-15 08:24:21 +0000

    arrays theorie

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -6,6 +6,21 @@
  * 
  */
 
+let arr;
+//arr = new Array(); // Konstruktor
+arr =[];
+arr = [2,11,7,8,10] // Array mit Elementen
+output (arr);
+output(arr.length)   //Anzahl der elemente durch .lenght
+output(arr[0])   // Index 0 ( 1. Pos. im Array)
+output(arr[arr.length-1])   // Index letzte Pos. (durch lenght-1 die aktuelle letzte posi)
+
+
+
+
+
+
+
 
 
 /**
@@ -16,15 +31,17 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 +
                 PUNCT;
 
     return str;
