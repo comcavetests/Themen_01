5cec3ae5269a5c8606763b64ef2d98f80727e1c2
TaKuu93 <168723110+TaKuu93@users.noreply.github.com>
2024-05-15 08:40:14 +0000

    Funktionalität mit Array 1

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -6,14 +6,14 @@
  * 
  */
 
-let arr;
-//arr = new Array(); // Konstruktor
-arr =[];
-arr = [2,11,7,8,10] // Array mit Elementen
-output (arr);
-output(arr.length)   //Anzahl der elemente durch .lenght
-output(arr[0])   // Index 0 ( 1. Pos. im Array)
-output(arr[arr.length-1])   // Index letzte Pos. (durch lenght-1 die aktuelle letzte posi)
+// let arr;
+// //arr = new Array(); // Konstruktor
+// arr =[];
+// arr = [2,11,7,8,10] // Array mit Elementen
+// output (arr);
+// output(arr.length)   //Anzahl der elemente durch .lenght
+// output(arr[0])   // Index 0 ( 1. Pos. im Array)
+// output(arr[arr.length-1])   // Index letzte Pos. (durch lenght-1 die aktuelle letzte posi)
 
 
 
@@ -48,12 +48,30 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 }
 
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
 
+
+//output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
 function output(outputStr) {
     console.log(outputStr);
 }
-
-
